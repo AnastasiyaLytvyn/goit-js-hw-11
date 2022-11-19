8972a2b35c8371c1fd18fba27cc71c8135a7fa51
AnastasiyaLytvyn <107053207+AnastasiyaLytvyn@users.noreply.github.com>
2022-11-19 21:35:18 +0200

    add changes

--- src/fetch.js
+++ src/fetch.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '31365887-26ab2d64c6ea6999261bbdea7'
 
-export async function fetchImg(data, page) {
+export async function fetchImg(r, page) {
   const apiData = await axios.get(
-    `${BASE_URL}?key=${API_KEY}&q=${data}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
+    `${BASE_URL}?key=${API_KEY}&q=${r}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
   )
   return apiData;
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,72 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
 import { fetchImg } from './fetch';
+import SimpleLightbox from 'simplelightbox';
+import simpleLightbox from 'simplelightbox';
 
+let getEl = selector => document.querySelector(selector);
+getEl('.search-form').addEventListener('submit', onSearch);
+getEl('.load-more').addEventListener('click', onLoadMore);
 
+let page = 1;
+let query = '';
+
+function onSearch(e) {
+  e.preventDefault();
+  const value = e.currentTarget.searchQuery.value;
+  console.log(value);
+  getEl('.gallery').innerHTML = '';
+  fetchImg(value, page)
+    .then(data => {
+      page = 1;
+      if (data.totalHits === 0) {
+        Notiflix.Notify.warning(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else {
+        markupGallery(data.hits);
+        simpleLightbox.refresh();
+        Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+        query = value;
+      }
+    })
+    .catch(error => console.log(error))
+    .finally(() => {
+      getEl('.search-form').reset();
+    });
+}
+
+function markupGallery(hits) {
+  getEl('.gallery').insertAdjacentHTML('beforeend', makeGallery(hits));
+}
+
+function makeGallery() {
+  `<a class="photo-link" href = ${largeImageURL}>
+    <div class="photo-card">
+      <img src=${webformatURL} alt=${tags} loading="lazy"  />
+      <div class="info">
+        <p class="info-item">
+          <b>Likes</b>
+          ${likes}
+        </p>
+        <p class="info-item">
+          <b>Views</b>
+          ${views}
+        </p>
+        <p class="info-item">
+          <b>Comments</b>
+          ${comments}
+        </p>
+        <p class="info-item">
+          <b>Downloads</b>
+          ${downloads}
+        </p>
+      </div>
+    </div>
+  </a>`;
+}
+
+function onLoadMore() {
+  page += 1;
+  fetchImg();
+}
