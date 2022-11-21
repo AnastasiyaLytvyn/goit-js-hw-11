2fbea964ba5b77a6518e717e39384ddff29a33b2
AnastasiyaLytvyn <107053207+AnastasiyaLytvyn@users.noreply.github.com>
2022-11-21 16:23:26 +0200

    add loadmore

--- src/index.js
+++ src/index.js
@@ -3,19 +3,22 @@ import Notiflix from 'notiflix';
 import { fetchImg } from './fetch';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import './styles.css'
+import './styles.css';
+import simpleLightbox from 'simplelightbox';
 
 let getEl = selector => document.querySelector(selector);
+getEl('.load-more').classList.add('is-hidden');
 getEl('.search-form').addEventListener('submit', onSearch);
 getEl('.load-more').addEventListener('click', onLoadMore);
 
 let page = 1;
 let query = '';
-const lightbox = new SimpleLightbox('.gallery a');
+let lightbox = new SimpleLightbox('.gallery a');
 
 function onSearch(e) {
   e.preventDefault();
   page = 1;
+  getEl('.load-more').classList.remove('is-hidden');
   const value = e.currentTarget.searchQuery.value;
   console.log(value);
 
@@ -32,6 +35,7 @@ function onSearch(e) {
     .then(data => {
       console.log(data.hits);
       if (data.totalHits === 0) {
+        getEl('.load-more').classList.add('is-hidden');
         Notiflix.Notify.warning(
           'Sorry, there are no images matching your search query. Please try again.'
         );
@@ -51,8 +55,37 @@ function onSearch(e) {
     });
 }
 
+function onLoadMore() {
+  page += 1;
+  fetchImg(query, page)
+    .then(data => {
+      getEl('.gallery').insertAdjacentHTML('beforeend', makeGallery(data.hits));
+      console.log(data.hits);
+      lightbox.refresh();
+
+      const totalPage = Math.ceil(data.totalHits / data.hits.length);
+
+      if (page >= totalPage) {
+        getEl('.load-more').classList.add('is-hidden');
+        Notiflix.Notify.failure(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }
+    })
+    .catch(error => console.log(error));
+}
+
 function makeGallery(hits) {
-  return hits.map(({webformatURL, tags, likes, views, comments, downloads}) => `<div class="photo-card">
+  return hits
+    .map(
+      ({
+        webformatURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `<div class="photo-card">
   <img src="${webformatURL}" alt="${tags}" loading="lazy" />
   <div class="info">
     <p class="info-item">
@@ -72,10 +105,7 @@ function makeGallery(hits) {
       ${downloads}
     </p>
   </div>
-</div>`).join('');
-}
-
-function onLoadMore() {
-  page += 1;
-  fetchImg();
+</div>`
+    )
+    .join('');
 }
